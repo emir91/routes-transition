c47f1c30b938e22c995400ce441bfd967ff72b38
emir.vranac <vranacemir@gmail.com>
2022-04-12 16:20:18 +0200

    add code to animate routes transition

--- src/About.js
+++ src/About.js
@@ -1,10 +1,18 @@
 import React, { useEffect } from "react"
-import { Link, NavLink, Outlet, useNavigate } from "react-router-dom"
-
+import {
+  Link,
+  NavLink,
+  Outlet,
+  useNavigate,
+  useLocation,
+} from "react-router-dom"
+import { CSSTransition, TransitionGroup } from "react-transition-group"
 import "./assets/styles/about.css"
+import "./assets/styles/fade.css"
 
 const About = () => {
   const navigate = useNavigate()
+  const location = useLocation()
 
   useEffect(() => {
     navigate("/about/shops")
@@ -21,7 +29,12 @@ const About = () => {
           Products
         </NavLink>
       </div>
-      <Outlet />
+      <TransitionGroup location={location}>
+        <CSSTransition key={location.key} classNames='fade' timeout={200}>
+          <Outlet />
+        </CSSTransition>
+      </TransitionGroup>
+
       <Link to='/'>
         <button className='about-button'>Back to Home</button>
       </Link>
